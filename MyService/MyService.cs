feb470eaadc08404eb93a0207bcfba82b0d1d80e
Hans Christian Falkenberg <hcf@tracid.com>
2014-11-12 16:44:58 +0100

    solution skeleton

---
+++ MyService/MyService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace MyService
+{
+    public partial class MyService : ServiceBase
+    {
+        public MyService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
